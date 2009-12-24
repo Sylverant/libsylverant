98d74769aeb39b9abe04d7d4882be360d2429779
Lawrence Sebald <ljsebald@users.sourceforge.net>
2009-12-24 13:58:20 +0000

    Fix line endings on this file....

--- src/encryption/psogc-crypt.c
+++ src/encryption/psogc-crypt.c
@@ -1,4 +1,6 @@
 // Reversed from the original PPC ASM by Fuzziqer Software, 2004 
+// Modified by Lawrence Sebald (2009) to run properly when compiled for a 64-bit
+// machine.
 
 #include <stdio.h>
 #include "sylverant/encryption.h"
@@ -8,8 +10,7 @@
 
 void CRYPT_GC_MixKeys(CRYPT_SETUP* cs)
 {
-    unsigned long r0,r3,r4,*r5,*r6,*r7;
-    r3 = (unsigned long)cs;
+    uint32_t r0,r4,*r5,*r6,*r7;
 
     cs->gc_block_ptr = cs->keys;
     r5 = cs->keys;
@@ -18,21 +19,21 @@ void CRYPT_GC_MixKeys(CRYPT_SETUP* cs)
 
     while (r6 != cs->gc_block_end_ptr)
     {
-        r0 = *(unsigned long*)r6;
+        r0 = *(uint32_t*)r6;
         r6++;
-        r4 = *(unsigned long*)r5;
+        r4 = *(uint32_t*)r5;
         r0 ^= r4;
-        *(unsigned long*)r5 = r0;
+        *(uint32_t*)r5 = r0;
         r5++;
     }
 
     while (r5 != cs->gc_block_end_ptr)
     {
-        r0 = *(unsigned long*)r7;
+        r0 = *(uint32_t*)r7;
         r7++;
-        r4 = *(unsigned long*)r5;
+        r4 = *(uint32_t*)r5;
         r0 ^= r4;
-        *(unsigned long*)r5 = r0;
+        *(uint32_t*)r5 = r0;
         r5++;
     }
 }
@@ -46,13 +47,13 @@ unsigned long CRYPT_GC_GetNextKey(CRYPT_SETUP* cs)
 
 void CRYPT_GC_CreateKeys(CRYPT_SETUP* cs,unsigned long seed)
 {
-    unsigned long x,y,basekey,*source1,*source2,*source3,temp;
+    uint32_t x,y,basekey,*source1,*source2,*source3,temp;
     basekey = 0;
 
     cs->gc_seed = seed;
 
     cs->gc_block_end_ptr = &(cs->keys[521]);
-    cs->gc_block_ptr = (unsigned long*)cs->keys;
+    cs->gc_block_ptr = (uint32_t*)cs->keys;
 
     for  (x = 0; x <= 16; x++)
     {
@@ -65,21 +66,21 @@ void CRYPT_GC_CreateKeys(CRYPT_SETUP* cs,unsigned long seed)
             else basekey = basekey & 0x7FFFFFFF;
         }
         *cs->gc_block_ptr = basekey;
-        cs->gc_block_ptr = (unsigned long*)((unsigned long)cs->gc_block_ptr + 4);
+        cs->gc_block_ptr = (uint32_t*)((uint8_t*)cs->gc_block_ptr + 4);
     }
     source1 = &(cs->keys[0]);
     source2 = &(cs->keys[1]);
-    cs->gc_block_ptr = (unsigned long*)((unsigned long)cs->gc_block_ptr - 4);
+    cs->gc_block_ptr = (uint32_t*)((uint8_t*)cs->gc_block_ptr - 4);
     (*cs->gc_block_ptr) = (((cs->keys[0] >> 9) ^ (*cs->gc_block_ptr << 23)) ^ cs->keys[15]);//cs->keys[15]);
     source3 = cs->gc_block_ptr;
-    cs->gc_block_ptr = (unsigned long*)((unsigned long)cs->gc_block_ptr + 4);
+    cs->gc_block_ptr = (uint32_t*)((uint8_t*)cs->gc_block_ptr + 4);
     while (cs->gc_block_ptr != cs->gc_block_end_ptr)
     {
         *cs->gc_block_ptr = (*source3 ^ (((*source1 << 23) & 0xFF800000) ^ ((*source2 >> 9) & 0x007FFFFF)));
-        cs->gc_block_ptr = (unsigned long*)((unsigned long)(cs->gc_block_ptr) + 4);
-        source1 = (unsigned long*)((unsigned long)source1 + 4);
-        source2 = (unsigned long*)((unsigned long)source2 + 4);
-        source3 = (unsigned long*)((unsigned long)source3 + 4);
+        cs->gc_block_ptr = (uint32_t*)((uint8_t*)(cs->gc_block_ptr) + 4);
+        source1 = (uint32_t*)((uint8_t*)source1 + 4);
+        source2 = (uint32_t*)((uint8_t*)source2 + 4);
+        source3 = (uint32_t*)((uint8_t*)source3 + 4);
     }
     CRYPT_GC_MixKeys(cs);
     CRYPT_GC_MixKeys(cs);
@@ -89,9 +90,9 @@ void CRYPT_GC_CreateKeys(CRYPT_SETUP* cs,unsigned long seed)
 
 void CRYPT_GC_CryptData(CRYPT_SETUP* c,void* data,unsigned long size)
 {
-    unsigned long *address_start,*address_end;
-    address_start = (unsigned long*)data;
-    address_end = (unsigned long*)((unsigned long)data + size);
+    uint32_t *address_start,*address_end;
+    address_start = (uint32_t*)data;
+    address_end = (uint32_t*)((uint8_t*)data + size);
     while (address_start < address_end)
     {
         *address_start = *address_start ^ CRYPT_GC_GetNextKey(c);
@@ -101,11 +102,11 @@ void CRYPT_GC_CryptData(CRYPT_SETUP* c,void* data,unsigned long size)
 
 void CRYPT_GC_DEBUG_PrintKeys(CRYPT_SETUP* cs,char* title)
 {
-    unsigned long x,y;
+    uint32_t x,y;
     printf("\n%s\n### ###+0000 ###+0001 ###+0002 ###+0003 ###+0004 ###+0005 ###+0006 ###+0007\n",title);
     for (x = 0; x < 66; x++)
     {
-        printf("%03d",x * 8);
+        printf("%03u",x * 8);
         for (y = 0; y < 8; y++) printf(" %08X",cs->keys[(x * 8) + y]);
         printf("\n");
     }
