38da0ad8922f2b3be5c020cd439f4b66bb7e5b1b
Lawrence Sebald <ljsebald@users.sourceforge.net>
2014-07-14 18:19:43 +0000

    Add (unused) reference counting code to libsylverant.
    This was originally going to be used somewhere in the code, but I came
    up with a different way of doing what I had in mind. So, this code may
    well never get used, but it'll live here anyway.

---
+++ include/sylverant/memory.h
@@ -0,0 +1,28 @@
+/*
+    This file is part of Sylverant PSO Server.
+
+    Copyright (C) 2014 Lawrence Sebald
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License version 3
+    as published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef SYLVERANT__MEMORY_H
+#define SYLVERANT__MEMORY_H
+
+#include <stddef.h>
+
+extern void *ref_alloc(size_t sz, void (*dtor)(void *));
+extern void *ref_retain(void *r);
+extern void *ref_release(void *r);
+
+#endif /* !SYLVERANT__MEMORY_H */
---
+++ src/utils/memory.c
@@ -0,0 +1,99 @@
+/*
+    This file is part of Sylverant PSO Server.
+
+    Copyright (C) 2014 Lawrence Sebald
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License version 3
+    as published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "sylverant/memory.h"
+
+/* So... Fair warning, this stuff is a bit ugly. It should work nicely enough
+   though... */
+
+/* Underlying structure that represents a reference object. This version is
+   not padded out to a nice size, which is taken care of below. */
+struct ref_unpadded {
+    void (*dtor)(void *);
+    uint32_t refcnt;
+    uint32_t magic;
+};
+
+#define USZ sizeof(struct ref_unpadded)
+#define PSZ 32
+#define RMAGIC 0x1BADC0DE
+
+/* Actual reference structure, to give us a nicely sized structure that we can
+   use for reference counting. */
+struct ref {
+    struct ref_unpadded r;
+    uint8_t padding[PSZ - USZ];
+};
+
+void *ref_alloc(size_t sz, void (*dtor)(void *)) {
+    struct ref *r;
+    uint8_t *ptr;
+
+    assert(PSZ == sizeof(struct ref));
+
+    /* Allocate space for the object and the reference counting overhead. */
+    if(!(r = (struct ref *)malloc(sz + PSZ)))
+        return NULL;
+
+    /* Fill in the reference couting data. */
+    r->r.dtor = dtor;
+    r->r.refcnt = 1;
+    r->r.magic = RMAGIC;
+
+    /* Return the actual pointer to the object. */
+    ptr = ((uint8_t *)r) + PSZ;
+    return (void *)ptr;
+}
+
+void *ref_retain(void *r) {
+    uint8_t *ptr = (uint8_t *)r;
+    struct ref *rf = (struct ref *)(ptr - PSZ);
+
+    /* Cowardly refuse to do anything if the magic isn't right. */
+    if(rf->r.magic != RMAGIC)
+        return NULL;
+
+    ++rf->r.refcnt;
+
+    return r;
+}
+
+void *ref_release(void *r) {
+    uint8_t *ptr = (uint8_t *)r;
+    struct ref *rf = (struct ref *)(ptr - PSZ);
+
+    /* Cowardly refuse to do anything if the magic isn't right. */
+    if(rf->r.magic != RMAGIC)
+        return NULL;
+
+    /* Decrement the reference count and deallocate the object, if needed. */
+    if(!--rf->r.refcnt) {
+        if(rf->r.dtor)
+            rf->r.dtor(r);
+
+        free(rf);
+        r = NULL;
+    }
+
+    return r;
+}
